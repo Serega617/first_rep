dd50e4366790641fa757517b71ac3d25fd966f9d
Serega617 <gordeevs254@gmail.com>
2025-07-04 19:11:44 +0300

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -38,7 +38,7 @@ class MainWindow(CTk):
         self.open_img_button = CTkButton(self, text="3", width=50, height=40, command=self.open_image)
         self.open_img_button.place(x=0, y=0)
 
-        
+        EMOJI
         self.emoji_button = CTkButton(self, text="😊", width=40, height=40, command=self.toggle_emoji_menu)
         self.emoji_button.place(x=0, y=0)
 
